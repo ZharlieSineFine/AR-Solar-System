e96dd9438b4bc503f766661cec1a8ad43e185eae
Zhao Changlin <huskydawg@foxmail.com>
2024-10-25 15:11:55 +0800

    Upload AR Solar System project

---
+++ AR Solar System/Library/Bee/artifacts/iOS/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,80 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void ChangeLookAtTarget_Start_m06D201600BA3DFA6F04025BF95D2E34FE5B20793 (void);
+extern void ChangeLookAtTarget_OnMouseDown_mFA1A450EFF3D2EDD0217CDD40D9E909941A78126 (void);
+extern void ChangeLookAtTarget__ctor_m78563A4F0986E6D898E3C92F0ED2801600F80EF1 (void);
+extern void LookAtTarget_Start_m01A20708A9EBB576BA81A56A9F70519CCE7FC215 (void);
+extern void LookAtTarget_Update_m20FEC5E1902C63BF17E49AF8F892462D866F8828 (void);
+extern void LookAtTarget__ctor_mAE3AAB49B9CE47F00D1E666E7F3BD76D2EFC8E37 (void);
+extern void Rotate_Update_m73D585515036D9B7AAD8336BFB8567283CE4C7E7 (void);
+extern void Rotate__ctor_m0EE5CC8EB699542BFC438DC3D547D39E442E9EE4 (void);
+extern void RotateAround_Start_m60D89C951DB8A59408D523814BE341066396CB6F (void);
+extern void RotateAround_Update_mD852F1A1CD216B2452356FD6238457192FB86D05 (void);
+extern void RotateAround__ctor_mD6104139482BD35BB738C6DE77947910AF27905E (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[13] = 
+{
+	ChangeLookAtTarget_Start_m06D201600BA3DFA6F04025BF95D2E34FE5B20793,
+	ChangeLookAtTarget_OnMouseDown_mFA1A450EFF3D2EDD0217CDD40D9E909941A78126,
+	ChangeLookAtTarget__ctor_m78563A4F0986E6D898E3C92F0ED2801600F80EF1,
+	LookAtTarget_Start_m01A20708A9EBB576BA81A56A9F70519CCE7FC215,
+	LookAtTarget_Update_m20FEC5E1902C63BF17E49AF8F892462D866F8828,
+	LookAtTarget__ctor_mAE3AAB49B9CE47F00D1E666E7F3BD76D2EFC8E37,
+	Rotate_Update_m73D585515036D9B7AAD8336BFB8567283CE4C7E7,
+	Rotate__ctor_m0EE5CC8EB699542BFC438DC3D547D39E442E9EE4,
+	RotateAround_Start_m60D89C951DB8A59408D523814BE341066396CB6F,
+	RotateAround_Update_mD852F1A1CD216B2452356FD6238457192FB86D05,
+	RotateAround__ctor_mD6104139482BD35BB738C6DE77947910AF27905E,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[13] = 
+{
+	6607,
+	6607,
+	6607,
+	6607,
+	6607,
+	6607,
+	6607,
+	6607,
+	6607,
+	6607,
+	6607,
+	9827,
+	6607,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	13,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
---
+++ AR Solar System/Library/Bee/artifacts/iOS/il2cppOutput/cpp/Il2CppCCalculateFieldValues2.cpp
@@ -0,0 +1,101 @@
+﻿#include "pch-cpp.hpp"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+
+
+struct CharU5BU5D_t799905CF001DD5F13F7DBB310181FC4D8B7D0AAB;
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+struct U3CPrivateImplementationDetailsU3E_t0F5473E849A5A5185A9F4C5246F0C32816C49FCA  : public RuntimeObject
+{
+};
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F  : public RuntimeObject
+{
+};
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F_marshaled_pinvoke
+{
+};
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F_marshaled_com
+{
+};
+struct Enum_t2A1A94B24E3B776EEF4E5E485E290BB9D4D072E2  : public ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F
+{
+};
+struct Enum_t2A1A94B24E3B776EEF4E5E485E290BB9D4D072E2_marshaled_pinvoke
+{
+};
+struct Enum_t2A1A94B24E3B776EEF4E5E485E290BB9D4D072E2_marshaled_com
+{
+};
+#pragma pack(push, tp, 1)
+struct __StaticArrayInitTypeSizeU3D156_t8F943B966C86A6EE928E7DD09AC901705B5B4785 
+{
+	union
+	{
+		struct
+		{
+			union
+			{
+			};
+		};
+		uint8_t __StaticArrayInitTypeSizeU3D156_t8F943B966C86A6EE928E7DD09AC901705B5B4785__padding[156];
+	};
+};
+#pragma pack(pop, tp)
+#pragma pack(push, tp, 1)
+struct __StaticArrayInitTypeSizeU3D72_t7580F73F43F9E8A0AEF16576EFBED059DF855A74 
+{
+	union
+	{
+		struct
+		{
+			union
+			{
+			};
+		};
+		uint8_t __StaticArrayInitTypeSizeU3D72_t7580F73F43F9E8A0AEF16576EFBED059DF855A74__padding[72];
+	};
+};
+#pragma pack(pop, tp)
+struct ConfigurationSaveMode_t7D0C554DA73F5D44E7DE4950E3F9004C20C71766 
+{
+	int32_t ___value__;
+};
+struct U3CPrivateImplementationDetailsU3E_t0F5473E849A5A5185A9F4C5246F0C32816C49FCA_StaticFields
+{
+	__StaticArrayInitTypeSizeU3D156_t8F943B966C86A6EE928E7DD09AC901705B5B4785 ___2F3CA28C79364A166B44585761089BC20C48161210721EBED47C7C2B87B3E3FD;
+	__StaticArrayInitTypeSizeU3D72_t7580F73F43F9E8A0AEF16576EFBED059DF855A74 ___85A0491B008223D657FC31CDF47490C6026D671A089165B6C7713FEB9CFB184C;
+};
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
+
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+IL2CPP_EXTERN_C const int32_t g_FieldOffsetTable6002[2] = 
+{
+	static_cast<int32_t>(offsetof(U3CPrivateImplementationDetailsU3E_t0F5473E849A5A5185A9F4C5246F0C32816C49FCA_StaticFields, ___2F3CA28C79364A166B44585761089BC20C48161210721EBED47C7C2B87B3E3FD)),static_cast<int32_t>(offsetof(U3CPrivateImplementationDetailsU3E_t0F5473E849A5A5185A9F4C5246F0C32816C49FCA_StaticFields, ___85A0491B008223D657FC31CDF47490C6026D671A089165B6C7713FEB9CFB184C)),};
+IL2CPP_EXTERN_C const int32_t g_FieldOffsetTable6006[4] = 
+{
+	static_cast<int32_t>(sizeof(RuntimeObject)),0,0,0,};
